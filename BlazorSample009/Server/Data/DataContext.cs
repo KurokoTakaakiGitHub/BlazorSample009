3e041e441a94f9da2d9bba9cbbb05b7b1d4b712d
KurokoTakaaki <takatakarx93@gmail.com>
2021-08-12 16:50:48 +0900

    データベース作成準備

---
+++ BlazorSample009/Server/Data/DataContext.cs
@@ -0,0 +1,49 @@
+﻿using BlazorSample009.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BlazorSample009.Server
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity
+                .HasKey(e => e.Id);
+
+                entity.Property(e => e.Id)
+                .HasColumnName("Id")
+                .IsRequired();
+
+                entity.Property(e => e.Name)
+                .HasColumnName("Name")
+                .IsRequired();
+
+                entity.Property(e => e.Price)
+                .HasColumnName("Price")
+                .IsRequired();
+
+            });
+
+            
+            //modelBuilder.Entity<Product>().HasData(
+            //    new Product(){ Id = "001", Name = "Book", Price = 1000 },
+            //    new Product() { Id = "002", Name = "Pen", Price = 500 },
+            //    new Product() { Id = "003", Name = "Laptop", Price = 30000 }
+            //    );
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ BlazorSample009/Shared/EntityProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorSample009.Shared
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+    }
+}
